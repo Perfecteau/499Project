d269674a7e35c6500795dafbd5642dacea0533ac
Perfecteau <cj_fecteau@hotmail.com>
2017-10-17 16:49:49 -0400

    Add files via upload

---
+++ core/init.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('/../config/paths.php');
+require_once('/../config/dbconfig.php');
+require_once('Session.php');
+require_once('App.php');
+require_once('Controller.php');
+require_once('Database.php');
